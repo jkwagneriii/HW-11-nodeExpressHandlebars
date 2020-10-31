d4183d1ad18635ffcd153076e0856258f39f87c4
Jake Wagner <jkwagneriii@gmail.com>
2020-10-31 15:56:25 -0500

    skeleton orm.js file

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+//methods that will execute the necessary MySQL commands in the controllers
+function selectAll();
+
+function insertOne();
+
+function updateOne();
+
+// Export the orm object for the model 
+module.exports = orm;

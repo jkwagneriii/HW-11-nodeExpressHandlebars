438deee8a7549c455e685ec2b850ec4338de007f
Jake Wagner <jkwagneriii@gmail.com>
2020-10-31 15:31:14 -0500

    working on file structure. json packages and schema.

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATA IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.

b9712c8323d8c5110e897f376fa69d69baf620a4
Jake Wagner <jkwagneriii@gmail.com>
2020-10-31 16:04:16 -0500

    skeleton controllers foler/file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/cat.js");
+
+
+// Create all our routes and set up logic within those routes where required.
+// router.get("/", function(req, res) {
+
+// });
+
+// Export routes for server.js to use.
+module.exports = router;

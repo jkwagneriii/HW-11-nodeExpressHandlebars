c101e4ef72025ffa2bb72098d54542e70ed1aa80
Jake Wagner <jkwagneriii@gmail.com>
2020-10-31 15:46:15 -0500

    added seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,11 @@ DROP DATA IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Kevin Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lots-A-Shroom', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Le Wagyu', false);

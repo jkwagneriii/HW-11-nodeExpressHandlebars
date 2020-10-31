d36f421ba0c1d1d8321eeb35f4be8b9d66b8709c
Jake Wagner <jkwagneriii@gmail.com>
2020-10-31 16:00:33 -0500

    skeleton models folder/file

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// Import the ORM to create functions that will interact with the database.
+let orm = require("../config/orm.js");
+
+//create the code that will call the ORM functions using burger specific input for the ORM
+let burger = {
+    
+};
+
+// Export the database functions for the controller 
+module.exports = burger;
